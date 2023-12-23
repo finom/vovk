50ac5f2f5433fb5232b717abf8aa62a7b0e534b7
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2023-12-23 22:06:23 +0100

    fix: Minor typing change

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://finom.github.io/',
+  url: 'https://docs.vovk.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'vovk-website/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
