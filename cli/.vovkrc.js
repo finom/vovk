a1f3e64eaa02527272bc7eb3b0c6ffd4b3a26946
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-01-14 19:34:49 +0100

    chore: Minor

--- cli/.vovkrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    route: 'src/app/api/[[...]]/route.ts',
-    fetcher: 'vovk/client/fetcher',
-    streamFetcher: 'vovk/client/streamFetcher',
-    prefix: 'api',
-    validateOnClient: 'vovk-zod/validateOnClient',
-}
