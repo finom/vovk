072e73f4d7cd8701189d0e07be1eb8dc2cc3b383
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-01-15 12:32:54 +0100

    fix: Minor

--- cli/postinstall.js
+++ cli/postinstall.js
@@ -6,7 +6,7 @@ const fileExists = async (path) => !!(await fs.stat(path).catch(() => false));
 async function postinstall() {
   const vovk = path.join(__dirname, '../../.vovk');
   const js = path.join(vovk, 'index.js');
-  const ts = path.join(vovk, 'index.ts');
+  const ts = path.join(vovk, 'index.d.ts');
 
   if ((await fileExists(js)) || (await fileExists(ts))) {
     return;
