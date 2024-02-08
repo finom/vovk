36adec9fccf52e5a7b2317a7df380186e36cd93f
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-08 21:11:42 +0100

    fix: Minor

--- cli/postinstall.mjs
+++ cli/postinstall.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {(path: string) => Promise<boolean>} */
 const fileExists = async (path) => !!(await fs.stat(path).catch(() => false));
