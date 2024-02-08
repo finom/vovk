ea50a5edb77b62a967d5f837ee552a0176cfcb60
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-02-08 21:13:22 +0100

    fix: Minor

--- cli/postinstall.mjs
+++ cli/postinstall.mjs
@@ -3,6 +3,8 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck Ignore meta-property error
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
