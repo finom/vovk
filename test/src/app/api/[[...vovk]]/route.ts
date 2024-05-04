818ee94e3f84a393dae1da728b338026f7ca65fd
Andrii Gubanov <andrey.a.gubanov@gmail.com>
2024-05-04 21:24:46 +0200

    test: Minor

--- test/src/app/api/[[...vovk]]/route.ts
+++ test/src/app/api/[[...vovk]]/route.ts
@@ -40,7 +40,7 @@ export const { GET, POST, PATCH, PUT, HEAD, OPTIONS, DELETE } = initVovk({
   },
   onError: (err, req) => {
     // eslint-disable-next-line no-console
-    console.log('onError', err.message, req);
+    console.log('onError', err.message, req.url);
   },
 });
 
