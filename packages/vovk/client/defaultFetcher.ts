1737fdbafafc77a4e81a573901269e4a1e7ecbe9
Andrii Gubanov <andrii@gubanov.eu>
2024-09-02 12:57:01 +0200

    fix(main): Minor

--- packages/vovk/client/defaultFetcher.ts
+++ packages/vovk/client/defaultFetcher.ts
@@ -11,7 +11,7 @@ export const DEFAULT_ERROR_MESSAGE = 'Unknown error at defaultFetcher';
 // For normal HTTP errors, it uses message and status code from the response of VovkErrorResponse type
 const defaultFetcher: VovkClientFetcher<VovkDefaultFetcherOptions> = async (
   { httpMethod, getEndpoint, validate, defaultHandler, defaultStreamHandler },
-  { params, query, body, prefix = '/api', segmentName, ...options }
+  { params, query, body, prefix = '/api', segmentName = '', ...options }
 ) => {
   const endpoint = getEndpoint({ prefix, segmentName, params, query });
 
--- packages/vovk/client/types.ts
+++ packages/vovk/client/types.ts
@@ -98,7 +98,7 @@ export type _VovkClientFetcher<OPTS extends Record<string, KnownAny> = Record<st
 export interface _VovkDefaultFetcherOptions extends Omit<RequestInit, 'body' | 'method'> {
   reactNative?: { textStreaming: boolean };
   prefix?: string;
-  segmentName: string;
+  segmentName?: string;
   disableClientValidation?: boolean;
   validateOnClient?: _VovkValidateOnClient;
   fetcher?: _VovkClientFetcher;
