7b6a85e4ffde03b8b0e8eb588df68a9a608b9204
Andrii Gubanov <andrii@gubanov.eu>
2025-02-04 21:53:31 +0100

    vovk-v3.0.0-draft.62

--- packages/vovk/src/client/defaultFetcher.ts
+++ packages/vovk/src/client/defaultFetcher.ts
@@ -51,7 +51,7 @@ const defaultFetcher: VovkClientFetcher<VovkDefaultFetcherOptions> = async (
     return defaultHandler(response);
   }
 
-  if (contentType === 'text/plain; format=jsonlines') {
+  if (contentType === 'text/plain; charset=utf-8') {
     return defaultStreamHandler(response);
   }
 
