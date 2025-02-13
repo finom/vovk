fad60f155255948641da3617a245442be7c38138
Andrey Gubanov <andrii@gubanov.eu>
2025-02-13 10:02:08 +0100

    vovk-openapi-v0.0.1-draft.21

--- packages/vovk-openapi/src/index.ts
+++ packages/vovk-openapi/src/index.ts
@@ -38,7 +38,7 @@ export const openapi = createDecorator(null, (openAPIOperationObject: OperationO
 const response = await MyRPC.${handlerName}(${
       hasArg
         ? `{
-    ${queryFake ? `query: ${stringifyData(queryFake)},\n` : ''}${bodyFake ? `body: ${stringifyData(bodyFake)},\n` : ''}${paramsFake ? `params: ${stringifyData(paramsFake)},\n` : ''}}`
+    ${queryFake ? `query: ${stringifyData(queryFake)},\n` : ''}${bodyFake ? `    body: ${stringifyData(bodyFake)},\n` : ''}${paramsFake ? `    params: ${stringifyData(paramsFake)},\n` : ''}}`
         : ''
     });
     ${
