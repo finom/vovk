05726ac393acf14639fa3daade327132364ed697
Andrey Gubanov <andrii@gubanov.eu>
2025-05-14 22:59:48 +0200

    fix: Missing files

---
+++ test/other-compiled-test-sources/segment-client/root/index.ts
@@ -0,0 +1,13 @@
+// auto-generated 2025-05-14T20:56:53.448Z
+/* eslint-disable */
+import type { VovkClientFetcher } from 'vovk';
+import { fetcher } from 'vovk';
+import { createRPC } from 'vovk';
+import { fullSchema } from './fullSchema.ts';
+
+
+import { validateOnClient } from '../../../../packages/vovk-ajv/index.js';
+
+type Options = typeof fetcher extends VovkClientFetcher<infer U> ? U : never;
+
+export { fullSchema };
