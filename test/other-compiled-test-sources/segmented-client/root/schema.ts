d0fac23ca410c1d714bbeb301927edf914aa982b
Andrey Gubanov <andrii@gubanov.eu>
2025-05-16 17:41:46 +0200

    chore: Prettier

--- test/other-compiled-test-sources/segmented-client/root/schema.ts
+++ test/other-compiled-test-sources/segmented-client/root/schema.ts
@@ -1,9 +1,8 @@
 // auto-generated 2025-05-15T21:59:09.447Z
 /* eslint-disable */
-import config from './../../../.vovk-schema/config.json' with { type: "json" };
+import config from './../../../.vovk-schema/config.json' with { type: 'json' };
 
-const segments = {
-};
+const segments = {};
 
 export const schema = {
   $schema: 'https://vovk.dev/api/spec/v3/schema.json',
