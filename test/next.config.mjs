75e63f46c704803400a0120a1d87e4678097433b
Andrey Gubanov <andrii@gubanov.eu>
2025-11-12 17:30:47 +0400

    chore: Make some asjustments after upgrading next to 16

--- test/next.config.mjs
+++ test/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* turbopack: {
+    root: import.meta.dirname,
+    resolveAlias: {
+      vovk: path.join(import.meta.dirname, '/../packages/vovk/src/index.ts'),
+    },
+  }, */
+};
 
 export default nextConfig;
