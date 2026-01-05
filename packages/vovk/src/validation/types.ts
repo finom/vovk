72971610da058877bf63599e4437c246ea00c473
Andrey Gubanov <andrii@gubanov.eu>
2026-01-05 11:57:00 +0400

    refactor(main): Remove unused types

--- packages/vovk/src/validation/types.ts
+++
@@ -1,4 +0,0 @@
-import type { StandardSchemaV1, StandardJSONSchemaV1 } from '@standard-schema/spec';
-
-export interface CombinedSchema<Input = unknown, Output = Input>
-  extends StandardSchemaV1.Props<Input, Output>, StandardJSONSchemaV1.Props<Input, Output> {}
